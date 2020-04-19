9545aff55ca3b3107b8b624c3fda432cf0578045
run-daft-cycle <robyn.ranford@gmail.com>
2020-04-19 11:07:51 +0100

    Documenting running CosmosDB locally

--- README.md
+++ README.md
@@ -12,15 +12,55 @@ git clone [this]
 
 >npm install -g lite-server
 
+### Local CosmosDB MongoDB instance 
 
-### Installing
+Dev set up
+Windows 10 Home Ed with Visual Studio Code
+If you don't have access to run the emulator in Docker and no access to Azure CosmosDB
 
-ToDo
+- Install Visual Studio Code CosmosDB Plugin
+- Download and install Microsoft CosmosDB Emulator
+- Download and install MongoDB 4.2
+-- Go through set up ensure its installed as a service
+- Re-start Windows machine
+- Start the Emulator with a data dir
+-- Create a dir for the data, say `C:\monogodbdata`
+-- Start the Empulator in that dir
+```bash
+cd C:\mongodbdata
+"C:\Program Files\Azure Cosmos DB Emulator\Microsoft.Azure.Cosmos.Emulator.exe" /EnableMongoDbEndpoint
+```
+- In Visual Studio Code
+-- Navigate to CosmosDB Plugin
+-- Right click "Attach CosmosDB for MongoDB"
+- Create your DB & a collection
+-- Right click MongoDB connection "Create database...", give it a name (see in code)
+-- Right click your database "Create collection...", give it a name (see in code)
+
+### Example local func setting local.settings.json
+
+```json
+{
+  "IsEncrypted": false,
+  "Values": {
+    "AzureWebJobsStorage": "",
+    "FUNCTIONS_WORKER_RUNTIME": "node",
+    "speakers_COSMOSDB": "mongodb://127.0.0.1:27017"
+  },
+  "Host" : {
+    "LocalHttpPort": 7071,
+    "CORS": "*"
+  }
+}
+```
 
 ## Run locally
 
 >cd frontend
 >lite-server
+
+>cd backend
+>func start
 
 ## Running the tests
 
@@ -37,3 +77,4 @@ ToDo
 
 ## License
 This project is licensed under the MIT License
+
--- backend/createSpeaker/index.js
+++ backend/createSpeaker/index.js
@@ -1,32 +1,41 @@
 const mongo = require("mongodb").MongoClient;
 
+console.log("START");
 module.exports = function(context, req) {
   context.log("createSpeaker function processing request");
   context.log("req.body", req.body);
-  if (req.body) {
-    let speakerData = req.body;
-    //connect to Mongo and list the items
-    mongo.connect(
-      process.env.speakers_COSMOSDB,
-      (err, client) => {
-        context.log(err);
-        context.log(client);
-        let send = response(client, context);
-        if (err) send(500, err.message);
-        let db = client.db("acloudguru");
-        db.collection("speakers").insertOne(speakerData, (err, speakerData) => {
+  
+  try {
+    if (req.body) {
+      let speakerData = req.body;
+      //connect to Mongo and list the items
+      mongo.connect(
+        process.env.speakers_COSMOSDB,
+        (err, client) => {
+          context.log(err);
+          context.log(client);
+          let send = response(client, context);
           if (err) send(500, err.message);
+          let db = client.db("acloudguru");
+          db.collection("speakers").insertOne(speakerData, (err, speakerData) => {
+            if (err) send(500, err.message);
 
-          send(200, speakerData);
-        });
-      }
-    );
-  } else {
-    context.res = {
-      status: 400,
-      body: "Please pass name in the body"
-    };
+            send(200, speakerData);
+          });
+        }
+      );
+    } else {
+      context.res = {
+        status: 400,
+        body: "Please pass name in the body"
+      };
+    }
+  } catch (err) {
+    context.log.error('ERROR', err);
+    // This rethrown exception will be handled by the Functions Runtime and will only fail the individual invocation
+    throw err;
   }
+  context.log(`COMPLETE`);
 };
 
 //Helper function to build the response
